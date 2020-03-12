6212557035324bcd12563763bf0f272c273a48c1
gustavo <tecnico.eletronico@yahoo.com.br>
2020-03-12 01:27:58 -0300

    vamosque

--- acumulador.c
+++ acumulador.c
@@ -1,1 +1,22 @@
-acumulador
+#include<stdio.h>
+int main(){
+	 float acumulador = 1,fatorial,controle,n;
+	printf("fatorial ");
+	scanf("%f",&fatorial);
+  
+		if(fatorial == 0){
+		fatorial = 1;
+	}
+while(fatorial>0){
+		acumulador = acumulador * fatorial;
+	
+		fatorial--;
+	}
+	
+	
+	printf("\t\t%f",acumulador);
+	
+	
+	
+	return 0;
+}
