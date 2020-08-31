2d789c9aa841dce639adb4bf0ef113cb6b98592a
Gustavo Seidel <gustavo.seidel@labtucal.ufsc.br>
2020-08-31 12:17:28 -0300

    limpando repositorio

--- final.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	printf("2,4,6,8,10,12,14,16,18,20");
-	
-	
-	return 0;
-}
--- teste1.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main (){
-	printf("2,4,6,8,10,12,14,18,22,26");
-	
-	
-	return 0;
-}
